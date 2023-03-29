2bf4695881db449c7d0dfd773f77c9a3a76ef29d
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-03-29 19:10:38 +0200

    Add LICENSE.txt and source file headers (MIT license)

--- src/settings_validator.erl
+++ src/settings_validator.erl
@@ -1,7 +1,15 @@
+%%
+%% Copyright (c) 2023, Byteplug LLC.
+%%
+%% This source file is part of the Settings Validator project from Erlangsters
+%% and is released under the MIT license. Please refer to the LICENSE.txt file
+%% that can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, March 2023
+%%
 -module(settings_validator).
 
 -export([settings_validator/0]).
 
 settings_validator() ->
     ok.
-
--- test/settings_validator_test.erl
+++ test/settings_validator_test.erl
@@ -1,7 +1,15 @@
+%%
+%% Copyright (c) 2023, Byteplug LLC.
+%%
+%% This source file is part of the Settings Validator project from Erlangsters
+%% and is released under the MIT license. Please refer to the LICENSE.txt file
+%% that can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, March 2023
+%%
 -module(settings_validator_test).
 
 -include_lib("eunit/include/eunit.hrl").
 
 settings_validator_test() ->
     ok.
-
